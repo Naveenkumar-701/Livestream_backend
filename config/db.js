b8c790411167a056a83493eb6a0a0c416a388d5a
Naveenkumar Ragupathy <naveen@arinnovate.io>
2025-11-17 18:57:27 +0530

    push

--- config/db.js
+++ config/db.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            dbName: "recroot",
-        });
-        console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
-        console.log(`📁 Using Database: ${conn.connection.name}`);
-    } catch (error) {
-        console.error(`❌ MongoDB Connection Error: ${error.message}`);
-        process.exit(1);
-    }
-};
+// const connectDB = async () => {
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI, {
+//             dbName: "recroot",
+//         });
+//         console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+//         console.log(`📁 Using Database: ${conn.connection.name}`);
+//     } catch (error) {
+//         console.error(`❌ MongoDB Connection Error: ${error.message}`);
+//         process.exit(1);
+//     }
+// };
 
-export default connectDB;
+// export default connectDB;
--- routes/lea_preview_routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createForm, getForms } from "../controllers/lea_preview_controller.js";
-
-const router = express.Router();
-
-router.post("/", createForm);
-router.get("/", getForms);
-
-export default router;
