067e3fe32ae86694f872b6af3e8d6c615cd8040d
preethi <preethi@arinnovate.io>
2025-12-21 22:19:17 +0530

    signup done

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -4,10 +4,14 @@ import jwt from "jsonwebtoken";
 // import User from "../models/User.js";
 // import { generateRefreshToken, generateSchedules } from "../utils/helpers.js";
 import { checkUser, loginWithoutPassword, sendEmployerOTPemail } from "../controllers/authController.js";
+import { registerUser, setPasswordAfterOtp, verifyOtp } from "../controllers/signupController.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/check-user", checkUser);
 authRouter.post("/login-no-password", loginWithoutPassword);
 authRouter.post("/send-otp", sendEmployerOTPemail);
+authRouter.post("/register", registerUser);
+authRouter.post("/verify-otp", verifyOtp);
+authRouter.post("/set-password", setPasswordAfterOtp);
 export default authRouter;
